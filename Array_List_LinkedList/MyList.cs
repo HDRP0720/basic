c829039db78c4a48d3a63f74057fd2c964a05976
HDRP <hdrp0720@gmail.com>
2023-03-05 15:04:45 +0900

    start project

---
+++ Array_List_LinkedList/MyList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_List_LinkedList
+{
+    // custom dynamic array
+    public class MyList<T>
+    {
+        private const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+
+        public int Count = 0;
+        public int Capacity { get { return _data.Length; } }
+
+        public void Add(T item)
+        {
+            if (Count >= Capacity)
+            {
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    newArray[i] = _data[i];
+                }
+                _data = newArray;
+            }
+
+            _data[Count] = item;
+            Count++;
+        }
+
+        // index
+        public T this[int index] 
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Count - 1] = default(T);
+            Count--;
+        }
+    }
+}
---
+++ Array_List_LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_List_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            int[] _data = new int[5];
+            MyList<int> _data2 = new MyList<int>();
+            NodeList<int> _data3 = new NodeList<int>();
+
+            #region example of usage
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            Node<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            _data3.Remove(node);
+            #endregion
+        }
+    }
+}
